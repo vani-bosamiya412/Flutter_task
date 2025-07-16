5320b90ebf471c6f6a181750ca9f752e65028b03
VaniBosamiya <128339420+Vanibos410@users.noreply.github.com>
2025-07-16 12:31:50 +0530

    c

--- Module-1/14-7/login_menu_bill.dart
+++ Module-1/14-7/login_menu_bill.dart
@@ -3,7 +3,7 @@ import 'dart:io';
 void main() {
   String uname = "tops";
   String pass = "1234";
-  
+
   print("Enter your username: ");
   String username = stdin.readLineSync().toString();
 
@@ -13,60 +13,62 @@ void main() {
   if(username != uname || password != pass) {
     print("Invalid username or password!");
     return;
-  }
+  } else {
+    print("Enter your table number: ");
+    int tno = int.parse(stdin.readLineSync().toString());
 
-  print("Enter your table number: ");
-  int tno = int.parse(stdin.readLineSync().toString());
+    int total = 0;
+    String bill = "---------------------BILL---------------------\n\nTable No: $tno\n\n";
 
-  print("---------------------MENU---------------------");
-  print("1. Pizza - Rs. 150");
-  print("2. Burger - Rs. 120");
-  print("3. Pasta - Rs. 100");
-  print("4. Sandwich - Rs. 90");
-  print("5. Fries - Rs. 70");
-  print("6. Cold Drink - Rs. 50");
+    while(true) {
+      print("---------------------MENU---------------------");
+      print("1. Pizza - Rs. 150");
+      print("2. Burger - Rs. 120");
+      print("3. Pasta - Rs. 100");
+      print("4. Sandwich - Rs. 90");
+      print("5. Fries - Rs. 70");
+      print("6. Cold Drink - Rs. 50");
+      print("7. Generate bill");
 
-  print("Enter your order: ");
-  String order = stdin.readLineSync().toString();
+      print("Enter your choice: ");
+      int choice = int.parse(stdin.readLineSync().toString());
 
-  int total = 0;
-  String bill = "---------------------BILL---------------------\n\nTable No: $tno\n\n";
-
-  for(int i = 0; i < order!.length; i++) {
-    String ch = order[i];
-
-    if (ch == ',') {
-      i++;
-      continue;
+      switch(choice) {
+        case 1:
+          bill += "\nPizza - Rs. 150";
+          total += 150;
+          break;
+        case 2:
+          bill += "\nBurger - Rs. 120";
+          total += 120;
+          break;
+        case 3:
+          bill += "\nPasta - Rs. 100";
+          total += 100;
+          break;
+        case 4:
+          bill += "\nSandwich - Rs. 90";
+          total += 90;
+          break;
+        case 5:
+          bill += "\nFries - Rs. 70";
+          total += 70;
+          break;
+        case 6:
+          bill += "\nCold Drink - Rs. 50";
+          total += 50;
+          break;
+        case 7:
+          bill += "\n\n----------------------------------------------\n\nTotal Bill: Rs.$total\n";
+          String path = "D:\\bill.txt";
+          var f = File(path);
+          f.writeAsString(bill);
+          print(bill);
+          print("Bill saved successfully to: $path");
+          return;
+        default:
+          print("Invalid item");
+      }
     }
-
-    switch(ch) {
-      case '1':
-        bill += "\nPizza - Rs. 150";
-        total += 150;
-      case '2':
-        bill += "\nBurger - Rs. 120";
-        total += 120;
-      case '3':
-        bill += "\nPasta - Rs. 100";
-        total += 100;
-      case '4':
-        bill += "\nSandwich - Rs. 90";
-        total += 90;
-      case '5':
-        bill += "\nFries - Rs. 70";
-        total += 70;
-      case '6':
-        bill += "\nCold Drink - Rs. 50";
-        total += 50;
-      default:
-        print("Invalid item");
-    }
   }
-  bill += "\n\n----------------------------------------------\n\nTotal Bill: Rs.$total\n";
-  String path = "D:\\bill.txt";
-  var f = File(path);
-  f.writeAsString(bill);
-  print(bill);
-  print("Bill saved successfully to: $path");
 }
